cab005978e3ebaac3640d53370be5cb98aba3ede
hrajmund <hubairajmund0507@gmail.com>
2023-10-22 10:54:51 +0200

    lab7

--- lab7/src/Commands.java
+++ lab7/src/Commands.java
@@ -3,13 +3,8 @@ import java.util.List;
 import java.util.*;
 
 public class Commands {
-    protected static void add(String cmd[], List<Beer> beers){
-        beers.add(new Beer(cmd[1], cmd[2], Double.parseDouble(cmd[3])));
-    }
-    protected static void exit(String[] cmd){
-        System.exit(0);
-    }
-    protected static void list(String[] cmd, List<Beer> beers){
+
+    /*protected static void list(String[] cmd, List<Beer> beers){
         Comparator<Beer> comparator;
         if(cmd.length > 1){
             switch (cmd[1]){
@@ -21,7 +16,7 @@ public class Commands {
             if(comparator == null){
                 System.out.println("nem jo");
             }else{
-                Collections.sort(beers, comparator);
+                Collections.sort(beers, (b1,b2) -> b1.getName().compareTo(b2.getName()));
             for(Beer item : beers){
                 System.out.println(item);
             }
@@ -33,68 +28,7 @@ public class Commands {
             }
         }
         
-    }
-    protected static List<Beer> load(String[] cmd, String file) throws IOException, ClassNotFoundException{
-        BufferedReader reader;
-        File f = new File(file, "beers.txt");
-        List<Beer> beers = new ArrayList<Beer>();
-        try{
-            reader = new BufferedReader(new FileReader(f));
-            String line;
-            while((line = reader.readLine()) != null){
-                String[] records = line.split(" ");
-                Beer tmp = new Beer(records[0], records[1], Double.parseDouble(records[2]));
-                beers.add(tmp);
-                System.out.println(tmp.toString());
-            }
-            reader.close();
-            return beers;
-        }catch (Exception e){
-            System.err.println(e);
-        }
+    }*/
 
-        return null;
-    }
 
-    protected static void save(String[] cmd, String file, List<Beer> beers) throws IOException{
-        String fw = System.getProperty("beers.txt");
-        File f = new File(file, "beers.txt");
-        System.out.println(f.getAbsolutePath());
-        FileWriter fwriter = null;
-        BufferedWriter writer = null;
-        try{
-            fwriter = new FileWriter(f);
-            writer = new BufferedWriter(fwriter);
-            for(Beer item : beers){
-                writer.write(item.toString() + "\n");
-            }
-        }catch (Exception e){
-            System.err.println(e);
-        }finally{
-            writer.close();
-            fwriter.close();
-        }
-    }
-    protected static void search(String[] cmd, List<Beer> beers){
-        for(Beer item : beers){
-            if (item.getName().equals(cmd[1])){
-                System.out.println(item.toString());
-            }
-        }
-    }
-    protected static void find(String[] cmd, List<Beer> beers){
-        for(Beer item : beers){
-            if(item.getName().contains(cmd[1])){
-                System.out.println(item.toString());
-            }
-        }
-    }
-    protected static void delete(String[] cmd, List<Beer> beers){
-        for(Beer item : beers){
-            if(item.getName().equals(cmd[1])){
-                beers.remove(item);
-                return;
-            }
-        }
-    }
 }
--- lab7/src/Main.java
+++ lab7/src/Main.java
@@ -1,13 +1,22 @@
-import java.io.BufferedReader;
-import java.io.File;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.List;
+import java.io.*;
+import java.util.*;
+
 public class Main extends Commands{
     public static List<Beer> beers = new ArrayList<Beer>();
-    public static void main(String[] args) throws IOException, ClassNotFoundException {
+    public static Map<String, Comparator<Beer>> comps;
+    public static List<String> lparams;
+    static{
+        comps = new HashMap<>();
+        lparams = new LinkedList<>();
 
+        comps.put("name", Comparator.comparing(Beer::getName));
+        comps.put("style", Comparator.comparing(Beer::getStyle));
+        comps.put("strength", Comparator.comparing(Beer::getStrength));
+
+        lparams.addAll(comps.keySet());
+    }
+    public static void main(String[] args) throws IOException, ClassNotFoundException {
+        HashMap<String, Command> commands = fillHashMap();
         String wd = System.getProperty("user.dir");
         File f = new File(wd);
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -15,24 +24,106 @@ public class Main extends Commands{
 
         while((line = br.readLine()) != null){
             String[] cmd = line.split(" ");
-            if(cmd[0].equals("add") && cmd.length == 4){
-                add(cmd, beers);
-            }else if(cmd[0].equals("exit")){
-                exit(cmd);
-            }else if(cmd[0].equals("list")){
-                list(cmd, beers);
-            }else if(cmd[0].equals("load")){
-                beers = load(cmd,wd);
-            }else if(cmd[0].equals("save")){
-                save(cmd, wd, beers);
-            }else if(cmd[0].equals("search")){
-                search(cmd, beers);
-            }else if(cmd[0].equals("find")){
-                find(cmd,beers);
-            }else if(cmd[0].equals("delete")){
-                delete(cmd,beers);
-            }
+            Command c = commands.get(cmd[0]);
+            c.execute(cmd);
         }
 
+    }
+    public static void list(String[] cmd){
+        if (cmd.length > 1) {
+            lparams.remove(cmd[1]);
+            lparams.add(0, cmd[1]);
+        }
+        Comparator<Beer>
+                c1 = comps.get(lparams.get(0)),
+                c2 = comps.get(lparams.get(1)),
+                c3 = comps.get(lparams.get(2));
+
+        beers.sort(c1.thenComparing(c2.thenComparing(c3)));
+
+        for (Beer beer : beers) {
+            System.out.println(beer);
+        }
+
+    }
+    public static void load(String[] cmd) throws IOException, ClassNotFoundException{
+        BufferedReader reader;
+        File f = new File(System.getProperty("user.dir"), "beers.txt");
+        try{
+            reader = new BufferedReader(new FileReader(f));
+            String line;
+            while((line = reader.readLine()) != null){
+                String[] records = line.split(" ");
+                Beer tmp = new Beer(records[0], records[1], Double.parseDouble(records[2]));
+                beers.add(tmp);
+                System.out.println(tmp.toString());
+            }
+            reader.close();
+
+        }catch (Exception e){
+            System.err.println(e);
+        }
+    }
+    public static void save(String[] cmd) throws IOException{
+        String fw = System.getProperty("beers.txt");
+        File f = new File(System.getProperty("user.dir"), "beers.txt");
+        System.out.println(f.getAbsolutePath());
+        FileWriter fwriter = null;
+        BufferedWriter writer = null;
+        try{
+            fwriter = new FileWriter(f);
+            writer = new BufferedWriter(fwriter);
+            for(Beer item : beers){
+                writer.write(item.toString() + "\n");
+            }
+        }catch (Exception e){
+            System.err.println(e);
+        }finally{
+            writer.close();
+            fwriter.close();
+        }
+    }
+    public static void search(String[] cmd){
+        for(Beer item : beers){
+            if (item.getName().equals(cmd[1])){
+                System.out.println(item.toString());
+            }
+        }
+    }
+    public static void find(String[] cmd){
+        for(Beer item : beers){
+            if(item.getName().contains(cmd[1])){
+                System.out.println(item.toString());
+            }
+        }
+    }
+    public static void delete(String[] cmd){
+        for(Beer item : beers){
+            if(item.getName().equals(cmd[1])){
+                beers.remove(item);
+                return;
+            }
+        }
+    }
+    public static void add(String cmd[]){
+        beers.add(new Beer(cmd[1], cmd[2], Double.parseDouble(cmd[3])));
+    }
+    public static void exit(String[] cmd){
+        System.exit(0);
+    }
+
+    public static HashMap<String, Command> fillHashMap() throws IOException, ClassNotFoundException{
+        HashMap<String, Command> tmp = new HashMap<>();
+
+        tmp.put("list", Main::list);
+        tmp.put("add", Main::add);
+        tmp.put("exit", Main::exit);
+        tmp.put("load", Main::load);
+        tmp.put("find", Main::find);
+        tmp.put("search", Main::search);
+        tmp.put("delete", Main::delete);
+        tmp.put("save", Main::save);
+
+        return tmp;
     }
 }
