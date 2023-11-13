818cc0db6e340b735011ffb5dd448b436a35dce6
Hubai Rajmund Szilveszter <hubairajmund0507@gmail.com>
2023-11-13 18:43:19 +0100

    test

---
+++ lab11/.idea/sonarlint/securityhotspotstore/index.pb
@@ -0,0 +1,13 @@
+
+=
+src/Main.java,9/2/924f089edb4af5ee45c8a9d1129cd28e2b368138
+T
+$src/test/junitlab/bank/BankTest.java,1/9/19cc67f4d630e326453555cb7e9abbee4c7bdda3
+K
+src/junitlab/bank/Bank.java,f/e/fe4a216e3b52a802524771d4e4055afa780d178f
+T
+$src/junitlab/bank/BankException.java,0/d/0dd6a0fb02b3fd23e5cec52cf2157d5cda9e4f89
+^
+.src/junitlab/bank/NotEnoughFundsException.java,f/0/f0091f59e97576f0fa0fdd6134f2bc7c6d190ad1
+`
+0src/junitlab/bank/AccountNotExistsException.java,e/d/edc5e3c690f6eee39917bb6d292c50966e9bfc32
---
+++ lab11/JUnitLab/test/junitlab/bank/BankTest.java
@@ -0,0 +1,35 @@
+package junitlab.bank;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import junitlab.bank.impl.GreatSavingsBank;
+
+public class BankTest {
+	GreatSavingsBank greatSavingsBank = new GreatSavingsBank();
+	
+	@Test
+	public void testOpenAccount() throws AccountNotExistsException {
+		String account = greatSavingsBank.openAccount();
+		Assert.assertEquals(0, greatSavingsBank.getBalance(account));
+	}
+
+	@Test
+	public void testUniqueAccount() {
+		String account1 = greatSavingsBank.openAccount();
+		String account2 = greatSavingsBank.openAccount();
+		Assert.assertNotEquals(account1, account2);
+	}
+
+	@Test(expected = AccountNotExistsException.class)
+	public void testInvalidAccount() throws AccountNotExistsException {
+		greatSavingsBank.getBalance("");
+	}
+
+	@Test
+	public void testDeposit() throws AccountNotExistsException {
+		String account = greatSavingsBank.openAccount();
+		greatSavingsBank.deposit(account, 2000);
+		Assert.assertEquals(2000, greatSavingsBank.getBalance(account));
+	}
+}
